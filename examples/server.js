97fb86ecfeaf86c9d63230c2f40e3d57c8a969fe
Joshua <lijianhuawork@163.com>
2020-12-25 15:29:38 +0800

    fix:post请求无返回问题

--- examples/server.js
+++ examples/server.js
@@ -7,6 +7,24 @@ const WebpackConfig = require('./webpack.config')
 
 const app = express()
 const compiler = webpack(WebpackConfig)
+
+app.use(
+  webpackDevMiddleware(compiler, {
+    publicPath: '/__build__/',
+    stats: {
+      colors: true,
+      chunks: false
+    }
+  })
+)
+
+app.use(webpackHotMiddleware(compiler))
+
+app.use(express.static(__dirname))
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+
 const router = express.Router()
 
 router.get('/simple/get', function(req, res) {
@@ -37,23 +55,6 @@ router.post('/base/buffer', function(req, res) {
 })
 
 app.use(router)
-
-app.use(
-  webpackDevMiddleware(compiler, {
-    publicPath: '/__build__/',
-    stats: {
-      colors: true,
-      chunks: false
-    }
-  })
-)
-
-app.use(webpackHotMiddleware(compiler))
-
-app.use(express.static(__dirname))
-
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: true }))
 
 const port = process.env.PORT || 8080
 module.exports = app.listen(port, () => {
